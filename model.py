4b89d9ec1551537aad9ee0d044ba85d7a0104513
Mouli Bhaskar <88495736+mouliduddupudi@users.noreply.github.com>
2022-08-28 00:06:42 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,122 @@
+import os
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.models as models
+
+
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embedding_size):
+        super(EncoderCNN, self).__init__()
+        
+        self.embedding_size = embedding_size
+        
+        self.inception = models.inception_v3(pretrained=True, aux_logits = False)
+        self.inception.fc = nn.Linear(self.inception.fc.in_features, embedding_size)    #in_features = 2048
+        
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        
+        
+    def forward(self, images):
+        
+        # images shape :: [batch size, channels, height, width]
+        features = self.inception(images)
+        #features shape :: [batch size, embedding size]
+        features = self.dropout(self.relu(features))
+        
+        return features
+    
+    
+    
+class DecoderRNN(nn.Module):
+    def __init__(self, embedding_size, hidden_size, vocab_size, num_layers):
+        super(DecoderRNN, self).__init__()
+        
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        self.lstm = nn.GRU(embedding_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.dropout = nn.Dropout(0.5)
+        
+    
+    def forward(self, features, captions):
+        
+        # features shape :: [batch size, embedding size]
+        embeddings = self.dropout(self.embedding(captions))
+
+        # embeeding shape :: [seq_length, batch size, embeeding size]
+        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
+
+        #h hiddens shape :: [seq length, batch size, hidden size]
+        hiddens, _ = self.lstm(embeddings)
+
+        # outputs shape :: [seq length, batch size, vocab size]
+        outputs = self.linear(hiddens)
+        
+        return outputs
+        
+
+
+class CNNtoRNN(nn.Module):
+    def __init__(self, embedding_size, hidden_size, vocab_size, num_layers):
+        super(CNNtoRNN, self).__init__()
+        
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        
+        self.encoder = EncoderCNN(embedding_size=embedding_size)
+        self.decoder = DecoderRNN(embedding_size=embedding_size, hidden_size=hidden_size, vocab_size=vocab_size, num_layers=num_layers)
+        
+    
+    def forward(self, images, captions):
+        
+        features = self.encoder(images)
+        outputs = self.decoder(features, captions)
+        
+        return outputs
+    
+
+    def caption_image(self, image, vocab, device, max_length=50):
+        result_caption = []
+        
+        with torch.no_grad():
+            image = image.unsqueeze(0)
+            image = image.to(device)
+            x = self.encoder(image)
+            x = x.unsqueeze(0)
+
+            states = None
+                        
+            for i in range(max_length):
+                
+                hiddens, states = self.decoder.lstm(x, states)
+                output = self.decoder.linear(hiddens.squeeze(0))
+                
+                predicted = output.argmax(1)
+                result_caption.append(predicted.item())
+                
+                x = self.decoder.embedding(predicted).unsqueeze(0)
+                
+                if vocab.itos[predicted.item()] == '<EOS>':
+                    break
+                
+            caption = [vocab.itos[x] for x in result_caption]
+            caption = ' '.join([id for id in caption])
+
+            caption = caption[6:-6]
+
+        return caption
+                
+        
+        
+        
+        
---
+++ train.py
@@ -0,0 +1,116 @@
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import pandas as pd
+import torchvision
+from torchvision.transforms import transforms
+from omegaconf import OmegaConf
+from argparse import ArgumentParser
+from torch.utils.tensorboard import SummaryWriter
+
+from data import FlickerDataset, get_dataloader, collate_fn, save_checkpoint
+from model import EncoderCNN, DecoderRNN, CNNtoRNN
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def train_fn(model, criterion, optimizer, train_dataloader, epochs):
+    
+    for name, param in model.encoder.inception.named_parameters():
+        if "fc.weight" or "fc.bias" in name:
+            param.requires_grad = True
+        
+        else:
+            param.requires_grad = False
+            
+    writer = SummaryWriter("runs/Flicker")
+    total_step = len(train_dataloader)
+
+    for epoch in tqdm(range(epochs)):
+        epoch_loss = 0
+
+        
+        for idx,(images, captions) in tqdm(enumerate(train_dataloader)):
+
+            images = images.to(device)
+        
+            captions = captions.to(device)
+            
+            outputs = model(images, captions[:-1])
+            
+            outputs = outputs.permute(0,2,1)
+
+            # outputs shape :: [seq length, vocab size, batch size]
+            # captions shape :: [seq length, batch size]
+
+            loss = criterion(outputs, captions)
+
+            epoch_loss += images.shape[0]* loss.item()
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+        epoch_avgloss = epoch_loss / len(train_dataloader.dataset)
+
+        writer.add_scalar("Training_loss", epoch_avgloss, global_step=epoch)
+
+        if epoch % 2 == 0:
+                print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch, epochs, epoch_avgloss))
+
+        
+        torch.save(model, 'model.pt')
+
+            
+
+
+if __name__ == '__main__':
+
+    root_dir = 'D:\projects\Image-Captioning-main\images'
+    captions_file = 'D:\projects\Image-Captioning-main\captions.txt'
+
+    transform = transforms.Compose([
+        transforms.Resize((255,255)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+    ])
+
+    print('started dataset')
+    dataset = FlickerDataset(root_dir=root_dir, captions_file=captions_file, transform=transform)
+    # vocab = torch.load("vocab.pt")
+    # print(vocab)
+    pad_idx = dataset.vocab.stoi['<PAD>']
+    print('started splitting')
+    # train_dataset, valid_dataset = train_test_split(dataset, test_size = 0.2, random_state=0)
+    train_dataset, valid_dataset = torch.utils.data.random_split(dataset, [35000, 5455])
+    print('ended splitting')
+    train_dataloader = DataLoader(train_dataset, batch_size=32, collate_fn=collate_fn(pad_idx=pad_idx))
+    valid_dataloader = DataLoader(valid_dataset, batch_size=32, collate_fn=collate_fn(pad_idx=pad_idx))
+
+    emb_size = 256
+    hidden_size = 256
+    vocab_size = len(dataset.vocab)
+    num_layers = 1
+    learning_rate = 0.001
+    epochs = 10
+            
+    model = CNNtoRNN(emb_size, hidden_size, vocab_size, num_layers)
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi['<PAD>'])
+    optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+    import time
+    print('started training')
+    start = time.time()
+    train_fn(model,criterion, optimizer, train_dataloader, epochs)
+    end = time.time()
+    print(end-start)
+
+            
+            
+            
+    
